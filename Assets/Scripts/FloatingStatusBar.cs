ed4cad2b73f252d7982c3820d6f4441f689e6da6
retroruiner <jostonfer@gmail.com>
2024-03-16 00:13:53 +0100

    Added health bar for an enemy

---
+++ Assets/Scripts/FloatingStatusBar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingStatusBar : MonoBehaviour
+{
+    [SerializeField]
+    private Slider slider;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateStatusBar(float currentValue, float maxValue)
+    {
+        slider.value = currentValue / maxValue;
+    }
+}
