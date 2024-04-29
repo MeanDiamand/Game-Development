dccda0e0f572b8e978587fd64b0b65ad75605e22
retroruiner <jostonfer@gmail.com>
2024-04-29 03:16:17 +0200

    Added level scene change but without a player

---
+++ Assets/Scripts/World/SwitchLevel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchLevel : MonoBehaviour
+{
+    public int sceneIndex;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Trigger enetered");
+
+        if (collision.tag == "Player")
+        {
+            print("Switching scene to: " + sceneIndex);
+            SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+        }
+    }
+}
