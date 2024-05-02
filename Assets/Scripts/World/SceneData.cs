8077c796fe478bbcadec163b423b2a4d9d48a5e7
CharNoir <59160057+CharNoir@users.noreply.github.com>
2024-05-02 19:16:17 +0200

    upd

---
+++ Assets/Scripts/World/SceneData.cs
@@ -0,0 +1,25 @@
+using Assets.Scripts;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu]
+public class SceneData : ScriptableObject
+{
+    [SerializeField]
+    private int sceneIndex;
+    [SerializeField]
+    private List<DamagableCharacter> enemies;
+    [SerializeField]
+    private List<Vector2> enemiesCoordinates;
+    [SerializeField]
+    private Vector2 spawnCoordinates;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+        PlayerEvents.GetInstance().Teleport(spawnCoordinates);
+
+        // TO-DO: Spawn enemies on defined coordinates
+    }
+}
--- Assets/Scripts/World/SwitchLevel.cs
+++ Assets/Scripts/World/SwitchLevel.cs
@@ -1,23 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class SwitchLevel : MonoBehaviour
 {
     [SerializeField]
-    private int sceneIndex;
-    [SerializeField]
-    private Vector2 newCoordinates;
+    private SceneData newScene;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            print("Switching scene to: " + sceneIndex);
-            SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
-            print("Teleporting: " + newCoordinates);
-            PlayerEvents.GetInstance().Teleport(newCoordinates);
+            newScene.LoadScene();
         }
     }
 }
