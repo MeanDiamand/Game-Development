9ef38b9b996194fe3f4947e1f9c9c792e2a617ef
CharNoir <59160057+CharNoir@users.noreply.github.com>
2024-05-02 19:32:09 +0200

    Initialization Scene added

--- Assets/Scripts/World/SwitchLevel.cs
+++ Assets/Scripts/World/SwitchLevel.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SwitchLevel : MonoBehaviour
@@ -5,11 +6,35 @@ public class SwitchLevel : MonoBehaviour
     [SerializeField]
     private SceneData newScene;
 
+    [SerializeField]
+    private bool autoSwitch;
+
+    [SerializeField]
+    private float switchDelay;
+
+    private void Start()
+    {
+        if (autoSwitch)
+        {
+            Debug.Log($"Scene started. Waiting {switchDelay} seconds until new scene starts.");
+            StartCoroutine(DelayedSceneLoad());
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
             newScene.LoadScene();
         }
+    }
+
+    private IEnumerator DelayedSceneLoad()
+    {
+        // Wait for the specified delay before loading the scene
+        yield return new WaitForSeconds(switchDelay);
+
+        // Load the new scene
+        newScene.LoadScene();
     }
 }
